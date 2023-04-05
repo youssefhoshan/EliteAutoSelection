79df18fcccc10eeb6fb40e4ddc79a76181451bd5
PatrickAgra <98827289+PatrickAgra@users.noreply.github.com>
2023-04-05 14:50:29 +0200

    Delete adminsystem directory

--- adminsystem/conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $conn = mysqli_connect("localhost", "root", "", "flower_power");
-    
-   
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-?>
--- adminsystem/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    	$id = $_GET['id'];
-    	include('./conn.php');
-    	mysqli_query($conn, "delete from `klant` Where id='$id' ");
-    	header('location:klanten.php');
-?>
